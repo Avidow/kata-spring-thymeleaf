ca4b43640dcd0301d27bda98ae7edbce302556a1
Daniel Aviidow <aviidow@gmail.com>
2022-11-29 15:04:32 +0300

    fix

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,17 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
 import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarsController {
 
-    CarService carService = new CarServiceImpl();
+    private CarService carService = new CarServiceImpl();
 
     @GetMapping(value = "/cars")
     public String carList(ModelMap model, @RequestParam(value = "count", required = false) Integer count) {
